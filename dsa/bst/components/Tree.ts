cb5ac4ae3479d0a9a42ae5bd1448cece4d29b61a
0xDev <169875752+devnchill@users.noreply.github.com>
2025-03-19 21:49:06 +0530

    feat: implement delete method

--- dsa/bst/components/Tree.ts
+++ dsa/bst/components/Tree.ts
@@ -4,6 +4,7 @@ class Tree {
   private root: MaybeNull<Node>;
 
   constructor(arr: Array<number>) {
+    //sort and remove the duplicates
     arr = [...new Set(arr)].sort((a, b) => a - b);
     this.root = this.buildTree(arr, 0, arr.length - 1);
   }
@@ -16,6 +17,7 @@ class Tree {
     if (start > end) return null;
     const mid = Math.floor((start + end) / 2);
     const root = new Node(arr[mid]);
+    //recursive call
     root.left = this.buildTree(arr, start, mid - 1);
     root.right = this.buildTree(arr, mid + 1, end);
     return root;
@@ -25,9 +27,41 @@ class Tree {
     if (root == null) {
       return new Node(value);
     } else if (value < root.data) {
+      //recursive call
       root.left = this.insert(value, root.left);
     } else {
+      //recursive call
       root.right = this.insert(value, root.right);
+    }
+    return root;
+  }
+
+  delete(value: number, root: MaybeNull<Node> = this.root): MaybeNull<Node> {
+    if (!root) return null;
+    // 1. Both children are null → Simply remove the node.
+    if (root.data == value && !root.right && !root.left) {
+      return null;
+    }
+    // 2. One child is null → Replace the node with its only child.
+    else if (root.data == value && (!root.left || !root.right)) {
+      if (!root.left) {
+        return root.right;
+      } else {
+        return root.left;
+      }
+    }
+    // 3. Both children exist → Replace with the leftmost node of the right subtree
+    if (root.data === value) {
+      let nextNode = root.right;
+      while (nextNode && nextNode.left) {
+        nextNode = nextNode.left;
+      }
+      if (nextNode) {
+        // Replace current node's value with the successor's value
+        root.data = nextNode.data;
+        // Delete the successor node (recursively)
+        root.right = this.delete(nextNode.data, root.right);
+      }
     }
     return root;
   }
